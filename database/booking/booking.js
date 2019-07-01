92c9c2d4f3d936d1e35d4641f7814287f4245567
lmavaiya <lmavaiya@gmail.com>
2019-07-01 22:53:48 +0530

    done

--- database/booking/booking.js
+++ database/booking/booking.js
@@ -2,15 +2,9 @@ var mongoose = require('mongoose');
 
 mongoose.model('Booking',{
     // _id: Number,
-<<<<<<< HEAD
-    client_id: Number,
-    worker_id: Number,
-    date_time: String,
-=======
     client_id: mongoose.Types.ObjectId,
     worker_id: mongoose.Types.ObjectId,
     service_type: mongoose.Types.ObjectId,
->>>>>>> 3be78c71f978476cbe755ae0dbce6c6e26afa87a
     address: String,
     status: Number,
     amount:Number,
