3be78c71f978476cbe755ae0dbce6c6e26afa87a
lmavaiya <lmavaiya@gmail.com>
2019-07-01 15:43:23 +0530

    database updated

--- database/admin/admin.js
+++ database/admin/admin.js
@@ -2,15 +2,27 @@ var mongoose = require('mongoose');
 
 // Create Schema and Model it
 mongoose.model("Admin", {
-    _id: Number,
-    email: String,
-    password: String,
+    // _id: mongoose.Schema.Types.ObjectId,
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        dropDups: true
+    },
+    password: {
+        type: String,
+        required: true,
+    }
 });
 
 
 mongoose.model("Services", {
-    _id: Number,
-    name: String,
-    avtar: String,
+    // _id: mongoose.Schema.Types.ObjectId,
+    name: {
+        type: String,
+        unique: true,
+        required: true,
+        dropDups: true
+    },
+    avatar: String,
 });
-
--- database/booking/booking.js
+++ database/booking/booking.js
@@ -1,11 +1,15 @@
 var mongoose = require('mongoose');
 
 mongoose.model('Booking',{
-    _id: Number,
-    client_id: Number,
-    worker_id: Number,
-    date_time: String,
+    // _id: Number,
+    client_id: mongoose.Types.ObjectId,
+    worker_id: mongoose.Types.ObjectId,
+    service_type: mongoose.Types.ObjectId,
     address: String,
     status: Number,
+    amount:Number,
+    request_date_time:Date,
+    date_time: Date,
+    close_date_time:Date,
 });
 
